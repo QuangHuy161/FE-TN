4470f44fe154cddc69ea21fb6cd884d98ad6abcf
QuangHuy161 <nguyenquanghuyh10a3#gmail.com>
2023-06-15 08:47:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express= require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const { Mongoose, default: mongoose } = require("mongoose");
 const Type = require("./models/typeModel");
 const Material =require("./models/materialModel");
 const User = require ("./models/userModel")
 const bcrypt = require("bcrypt");
+var bodyParser = require('body-parser');
 
 
 const app = express();
 
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 var options = {
   useNewUrlParser: true,
@@ -19,16 +21,16 @@ var options = {
 var url ="mongodb+srv://quanghuy161:Tmh5OdLzIzmh9Yup@cluster0.3wcmiob.mongodb.net/";
 
 const database = module.exports = () =>{
-    try {
-        mongoose.connect(
-            url,
-            options,
-        )
-        console.log("Database connected successfully ")
-    }catch (error){
-        console.log(error)
-        console.log('Database connection failed')
-    }
+  try {
+    mongoose.connect(
+        url,
+        options,
+    )
+    console.log("Database connected successfully ")
+  }catch (error){
+    console.log(error)
+    console.log('Database connection failed')
+  }
 }
 
 database();
@@ -42,40 +44,69 @@ const Nhomvattu = mongoose.model("Nhomvattu",Type)
 const Vattu = mongoose.model("Material",Material)
 
 app.get('/donvi' ,(req,res) =>{
-    res.header("Access-Control-Allow-Origin", "*");
-    const donvi = Donvi.find({}).then(items => {
-        res.json(items)
-    })
+  res.header("Access-Control-Allow-Origin", "*");
+  const donvi = Donvi.find({}).then(items => {
+      res.json(items)
+  })
 })
+app.get('/donvi/:_id' ,(req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const donvi = Donvi.find({_id: req.params._id}).then(items => {
+      res.json(items)
+  })
+})
+
 
 app.get('/nhomvattu' ,(req,res) =>{
-    res.header("Access-Control-Allow-Origin", "*");
-    const nhomvattu = Nhomvattu.find({}).then(items => {
-        res.json(items)
-    })
-   
+  res.header("Access-Control-Allow-Origin", "*");
+  const nhomvattu = Nhomvattu.find({}).then(items => {
+      res.json(items)
+  })
+})
+app.get('/nhomvattu/:_id' ,(req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const nhomvattu = Nhomvattu.find({_id: req.params._id}).then(items => {
+      res.json(items)
+  })
 })
 
+
 app.post('/add_data', async (req,res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    const id = req.body._id;
-    const ten = req.body.label;
-    const donvi = req.body.donvi;
-    const nhomvattu = req.body.nhomvattu;
-    const img = req.body.img;
-    const giatri = req.body.giatri;
-    const tien = req.body.gia;
-    const vattu = new Vattu(req.body);
-    vattu.save();
+  res.header("Access-Control-Allow-Origin", "*");
+  const vattu = new Vattu(req.body);
+  vattu.save();
+}) 
+
+app.post('/add_donvi', async (req,res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const donvi = new Donvi(req.body);
+  donvi.save();
+}) 
+app.post('/add_nhomvattu', async (req,res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  const nhomvattu = new Nhomvattu(req.body);
+  nhomvattu.save();
 }) 
 
 app.get('/list_mon',(req,res) =>{
   res.header("Access-Control-Allow-Origin", "*");
-    const Nguyenlieu = Vattu.find({}).then(items => {
-        res.json(items)
-    })
-   
+  const Nguyenlieu = Vattu.find({}).then(items => {
+      res.json(items)
+  })
 })
+app.get('/list_mon/:_id',(req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const Nguyenlieu = Vattu.find({_id: req.params._id}).then(items => {
+      res.json(items)
+  })
+})
+
+app.get('/update/list_mon/:_id',(req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const Nguyenlieu = Vattu.update()
+})
+
+
 
 app.post('/auth/signup', (req,res) =>{
     res.header("Access-Control-Allow-Origin", "*");
