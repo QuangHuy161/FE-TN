395daae7e66d01f644ff43d1e6e8bb5079f5551e
QuangHuy161 <nguyenquanghuyh10a3#gmail.com>
2023-07-11 00:20:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { Mongoose, default: mongoose } = require("mongoose");
 const Type = require("./models/typeModel");
 const Material =require("./models/materialModel");
 const User = require ("./models/userModel")
+const Mon = require("./models/monModel")
 const bcrypt = require("bcrypt");
 var bodyParser = require('body-parser');
-
 
 const app = express();
 
@@ -20,7 +20,7 @@ var options = {
 
 var url ="mongodb+srv://quanghuy161:Tmh5OdLzIzmh9Yup@cluster0.3wcmiob.mongodb.net/";
 
-const database = module.exports = () =>{
+const databaseConnection = module.exports = () =>{
   try {
     mongoose.connect(
         url,
@@ -32,104 +32,235 @@ const database = module.exports = () =>{
     console.log('Database connection failed')
   }
 }
+const closeConnection = module.exports = () =>{
+  try {
+    mongoose.connection.close();
+    console.log("Database closed ")
+  }catch (error){
+    console.log(error)
+    console.log('Database closed failed')
+  }
+}
 
-database();
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.options("/update/list_mon", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.sendStatus(204);
+});
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.get('/' ,(req,res) =>{
 })
 
+databaseConnection()
 const Donvi = mongoose.model("Donvi",Type)
 const Nhomvattu = mongoose.model("Nhomvattu",Type)
 const Vattu = mongoose.model("Material",Material)
+const Monan = mongoose.model("Mon",Mon)
 
 app.get('/donvi' ,(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
   const donvi = Donvi.find({}).then(items => {
       res.json(items)
-  })
+  }).then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
 })
 app.get('/donvi/:_id' ,(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
   const donvi = Donvi.find({_id: req.params._id}).then(items => {
       res.json(items)
-  })
+  }).then(function(){
+    // Success
+  }).catch(function(error){
+      console.log(error); // Failure
+  });
+  
 })
 
 
 app.get('/nhomvattu' ,(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
   const nhomvattu = Nhomvattu.find({}).then(items => {
       res.json(items)
-  })
+  }).then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
 })
 app.get('/nhomvattu/:_id' ,(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
   const nhomvattu = Nhomvattu.find({_id: req.params._id}).then(items => {
       res.json(items)
   })
+  .then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
 })
 
 
 app.post('/add_data', async (req,res) => {
+  
   res.header("Access-Control-Allow-Origin", "*");
   const vattu = new Vattu(req.body);
-  vattu.save();
+  vattu.save()
+  .then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
 }) 
 
 app.post('/add_donvi', async (req,res) => {
+  
   res.header("Access-Control-Allow-Origin", "*");
   const donvi = new Donvi(req.body);
-  donvi.save();
+  donvi.save()
+  .then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
 }) 
 app.post('/add_nhomvattu', async (req,res) => {
+  
   res.header("Access-Control-Allow-Origin", "*");
   const nhomvattu = new Nhomvattu(req.body);
-  nhomvattu.save();
+  nhomvattu.save()
+  .then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
 }) 
 
 app.get('/list_mon',(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
   const Nguyenlieu = Vattu.find({}).then(items => {
       res.json(items)
-  })
+  }).then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
 })
 app.get('/list_mon/:_id',(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
   const Nguyenlieu = Vattu.find({_id: req.params._id}).then(items => {
       res.json(items)
-  })
+  }).then(function(){
+     // Success
+  }).catch(function(error){
+      console.log(error); // Failure
+  });
+  
 })
 
-app.get('/update/list_mon/:_id',(req,res) =>{
+app.put('/update/list_mon/:_id',(req,res) =>{
+  
   res.header("Access-Control-Allow-Origin", "*");
-  const Nguyenlieu = Vattu.update()
+  const reqData= req.body;
+  var myQuerry = {_id:req.params._id}
+  var newValues = {
+    $set:reqData
+  }
+
+  Vattu.updateOne(myQuerry,newValues)
+  .then(function(){
+    res.send({status:"Updated"}); // Success
+  }).catch(function(error){
+      console.log(error); // Failure
+  });
+  
+})
+app.post('/delete/list_mon/:_id',(req,res) =>{
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  var myQuerry = {_id:req.params._id};
+
+  Vattu.deleteOne(myQuerry).then(function(){
+    res.send({status:"deleted"}); // Success
+  }).catch(function(error){
+      console.log(error); // Failure
+  });
+  
 })
 
+app.post('/themmon', (req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  let mon = new Monan(req.body)
+  mon.save()
+})
+app.get('/mon', (req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const mon = Monan.find({}).then(items => {
+    res.json(items)
+}).then(function(){
+  // Success
+}).catch(function(error){
+   console.log(error); // Failure
+});
+})
+
+
+app.get('/menu', (req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const menu = Vattu.find({nhomvattu: {$in:["Món","Topping"]}}).then(items => {
+      res.json(items)
+  }).then(function(){
+     // Success
+  }).catch(function(error){
+      console.log(error); // Failure
+  });
+
+})
 
 
 app.post('/auth/signup', (req,res) =>{
-    res.header("Access-Control-Allow-Origin", "*");
-    bcrypt.hash(req.body.password, 10)
-    .then((hashedPassword) => {
-        console.log(hashedPassword)
-        const user = new User({
-            fullname: req.body.fullname,
-            email: req.body.email,
-            password: hashedPassword,
-        });
-        user.save().then((result) => {
-            res.status(201).send({
-              message: "User Created Successfully",
-              result,
-            });
-          })
-          .catch((error) => {
-            res.status(500).send({
-              message: "Error creating user",
-              error,
-            });
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  bcrypt.hash(req.body.password, 10)
+  .then((hashedPassword) => {
+      console.log(hashedPassword)
+      const user = new User({
+          fullname: req.body.fullname,
+          email: req.body.email,
+          password: hashedPassword,
+      });
+      user.save().then((result) => {
+          res.status(201).send({
+            message: "User Created Successfully",
+            result,
           });
+        })
+        .catch((error) => {
+          res.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
   })
   .catch((e) => {
     res.status(500).send({
@@ -137,6 +268,7 @@ app.post('/auth/signup', (req,res) =>{
       e,
     });
   });
+  
 })
 
 app.listen (5000, () =>{
