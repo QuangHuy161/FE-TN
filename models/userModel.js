ff7313e0c3b1454bbb13ec1e6f5f747d4c1e11ca
QuangHuy161 <nguyenquanghuyh10a3#gmail.com>
2023-07-17 20:26:28 +0700

    Update user moddel

--- models/userModel.js
+++ models/userModel.js
@@ -13,22 +13,11 @@ const userSchema = mongoose.Schema(
             type: String,
             require: [true, "Please enter your name"]
         },
-        mail:{
-            type: String,
-            trim: true,
-            lowercase: true,
-            unique: true,
-            required: 'Email address is required',
-            validate: [validateEmail, 'Please fill a valid email address'],
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-        },
         password:{
             type:String,
             require: true
-        },
-        updated: { type: Date, default: Date.now },
+        }
     }
 )
 
-const UserModel = mongoose.model('User',userSchema);
-module.exports = UserModel;
+module.exports = userSchema;
