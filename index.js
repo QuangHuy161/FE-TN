50b4b9cb4cdd426308a197e418b2ca6930cef55b
QuangHuy161 <nguyenquanghuyh10a3#gmail.com>
2023-07-17 20:25:49 +0700

    update auth

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express= require("express");
 const cors = require("cors");
 const { Mongoose, default: mongoose } = require("mongoose");
-const Type = require("./models/typeModel");
-const Material =require("./models/materialModel");
-const User = require ("./models/userModel")
-const Mon = require("./models/monModel")
-const Order= require("./models/OrderDetailModel")
 const bcrypt = require("bcrypt");
 var bodyParser = require('body-parser');
+const jwt = require("jsonwebtoken");
+
+
+const Type = require("./models/typeModel");
+const Material =require("./models/materialModel");
+const User_Schema = require ("./models/userModel")
+const Mon = require("./models/monModel")
+const Order= require("./models/OrderDetailModel")
+const auth = require("./auth");
+
 
 const app = express();
 
@@ -26,9 +31,23 @@ app.options("/update/list_mon", (req, res) => {
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.get('/' ,(req,res) =>{
 })
 
+//=============DB connect==============
 var options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -62,12 +81,17 @@ const closeConnection = module.exports = () =>{
 
 
 databaseConnection()
+
+//=======MODELS
+const User =mongoose.model("User",User_Schema)
 const Donvi = mongoose.model("Donvi",Type)
 const Nhomvattu = mongoose.model("Nhomvattu",Type)
 const Vattu = mongoose.model("Material",Material)
 const Monan = mongoose.model("Mon",Mon)
 const OrderDetail = mongoose.model("Order",Order)
 
+///===============API================
+//=====DONVI
 app.get('/donvi' ,(req,res) =>{
   
   res.header("Access-Control-Allow-Origin", "*");
@@ -92,8 +116,19 @@ app.get('/donvi/:_id' ,(req,res) =>{
   });
   
 })
-
-
+app.post('/add_donvi', async (req,res) => {
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  const donvi = new Donvi(req.body);
+  donvi.save()
+  .then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
+}) 
+//=====NHOMVATTU
 app.get('/nhomvattu' ,(req,res) =>{
   
   res.header("Access-Control-Allow-Origin", "*");
@@ -118,8 +153,19 @@ app.get('/nhomvattu/:_id' ,(req,res) =>{
      console.log(error); // Failure
  });
 })
-
-
+app.post('/add_nhomvattu', async (req,res) => {
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  const nhomvattu = new Nhomvattu(req.body);
+  nhomvattu.save()
+  .then(function(){
+    // Success
+ }).catch(function(error){
+     console.log(error); // Failure
+ });
+  
+}) 
+//=====ADD_VATTU
 app.post('/add_data', async (req,res) => {
   
   res.header("Access-Control-Allow-Origin", "*");
@@ -133,30 +179,6 @@ app.post('/add_data', async (req,res) => {
   
 }) 
 
-app.post('/add_donvi', async (req,res) => {
-  
-  res.header("Access-Control-Allow-Origin", "*");
-  const donvi = new Donvi(req.body);
-  donvi.save()
-  .then(function(){
-    // Success
- }).catch(function(error){
-     console.log(error); // Failure
- });
-  
-}) 
-app.post('/add_nhomvattu', async (req,res) => {
-  
-  res.header("Access-Control-Allow-Origin", "*");
-  const nhomvattu = new Nhomvattu(req.body);
-  nhomvattu.save()
-  .then(function(){
-    // Success
- }).catch(function(error){
-     console.log(error); // Failure
- });
-  
-}) 
 
 app.get('/list_mon',(req,res) =>{
   
@@ -226,6 +248,9 @@ app.post('/delete/list_mon/:_id',(req,res) =>{
   });
 })
 
+
+
+//=========Tạo công thức và món
 app.post('/themmon', (req,res) =>{
   res.header("Access-Control-Allow-Origin", "*");
   console.log(req.body)
@@ -252,24 +277,13 @@ app.post('/delete/mon/:_id',(req,res) =>{
   });
 })
 
+
+//======Chi tiết order
 app.post('/add_order_detail', (req,res) =>{
   res.header("Access-Control-Allow-Origin", "*");
   const order = new OrderDetail(req.body)
   order.save()
 })
-
-app.get('/menu', (req,res) =>{
-  res.header("Access-Control-Allow-Origin", "*");
-  const menu = Vattu.find({nhomvattu: {$in:["Món","Topping"]}}).then(items => {
-      res.json(items)
-  }).then(function(){
-     // Success
-  }).catch(function(error){
-      console.log(error); // Failure
-  });
-
-})
-
 
 app.get("/filter_order/:t",(req,res)  =>{
   res.header("Access-Control-Allow-Origin", "*");
@@ -307,15 +321,28 @@ app.get("/filter_order/day/:t",(req,res)  =>{
       console.log(error); 
   });
 })
+
+//========MENU món và topping
+app.get('/menu', (req,res) =>{
+  res.header("Access-Control-Allow-Origin", "*");
+  const menu = Vattu.find({nhomvattu: {$in:["Món","Topping"]}}).then(items => {
+      res.json(items)
+  }).then(function(){
+     // Success
+  }).catch(function(error){
+      console.log(error); // Failure
+  });
+
+})
+
+
 app.post('/auth/signup', (req,res) =>{
-  
   res.header("Access-Control-Allow-Origin", "*");
   bcrypt.hash(req.body.password, 10)
   .then((hashedPassword) => {
       console.log(hashedPassword)
       const user = new User({
           fullname: req.body.fullname,
-          email: req.body.email,
           password: hashedPassword,
       });
       user.save().then((result) => {
@@ -339,6 +366,74 @@ app.post('/auth/signup', (req,res) =>{
   });
   
 })
+
+
+// login endpoint
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ fullname: request.body.fullname })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+
+          // check if password matches
+          if(!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userFullname: user.fullname,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: "Login Successful",
+            fullname: user.fullname,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Fullname not found",
+        e,
+      });
+    });
+});
+
+// free endpoint
+app.get("/free", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth",auth, (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
+
+
 
 app.listen (5000, () =>{
     console.log("serer is running on port 5000,http://localhost:5000/")
